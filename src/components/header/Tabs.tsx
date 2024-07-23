5a0c6c56e3591a718f855df95ecadf7a4f821219
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-07-23 21:54:45 +0100

    fix: tabs working, with ellipses showing, turns out this does not work in a flex context

--- src/components/header/Tabs.tsx
+++ src/components/header/Tabs.tsx
@@ -10,7 +10,8 @@ interface TabsProps {
 
 const Tabs: React.FC = () => {
   const [tabs, setTabs] = useState(["Tab 1"]);
-  const [hoveredTab, setHoveredTab] = useState<number | null>(null);
+  const [hoveredTab, setHoveredTab] = useState<number | null>(null); //to control the styling of the tab we hover over
+  const [activeTab, setActiveTab] = useState<number | null>(null);
 
   const addNewTab = () => {
     setTabs([...tabs, `Tab ${tabs.length + 1}`]);
@@ -20,8 +21,6 @@ const Tabs: React.FC = () => {
     setTabs(tabs.filter((_, i) => i !== index));
   };
 
-  console.log("Hovered tab:", hoveredTab);
-
   return (
     // outer div ensures that the tabs take up the remaining space in whatever view we use
     <div className="flex-1 overflow-hidden">
@@ -30,30 +29,30 @@ const Tabs: React.FC = () => {
           //  outer div so that when you hover over tab, it does not colour all the way to tab border
           <div
             key={index}
-            className="flex basis-40 justify-center items-center h-8 border-r px-1 border-gray-300 whitespace-nowrap overflow-hidden text-ellipsis"
+            className="flex basis-40 justify-center items-center h-8 border-r px-1 border-gray-300 overflow-hidden"
           >
             <div
-              className="h-7 basis-36 flex items-center justify-between text-sm hover:bg-slate-300 rounded"
+              className="h-7 basis-36 flex items-center justify-between text-sm hover:bg-slate-300 rounded overflow-hidden"
               onMouseEnter={() => setHoveredTab(index)}
               onMouseLeave={() => setHoveredTab(null)}
               title={tab}
             >
-              <span>{tab}</span>
-              <button
-                // if this is the button we are hovering over, show close button
-                className={`${
-                  index === hoveredTab
-                    ? "justify-center items-center flex-shrink-0"
-                    : "hidden"
-                }`}
-                onClick={() => closeTab(index)}
-              >
-                <img
-                  src={CloseIcon}
-                  alt="Close tab"
-                  className="h-5 w-5 flex-shrink-0"
-                />
-              </button>
+              <span className="whitespace-nowrap overflow-hidden text-ellipsis">
+                {tab}
+              </span>
+              {index === hoveredTab && (
+                <button
+                  // if this is the button we are hovering over, show close button
+                  className="justify-center items-center flex-shrink-0"
+                  onClick={() => closeTab(index)}
+                >
+                  <img
+                    src={CloseIcon}
+                    alt="Close tab"
+                    className="h-5 w-5 flex-shrink-0"
+                  />
+                </button>
+              )}
             </div>
           </div>
         ))}
