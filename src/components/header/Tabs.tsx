0317b3cdd27b941fc6b6cbdbb2aed1485fe76f46
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-07-27 13:23:40 +0100

    Feat: closing tabs now working and implmeneted

--- src/components/header/Tabs.tsx
+++ src/components/header/Tabs.tsx
@@ -12,20 +12,27 @@ const Tabs: React.FC = () => {
   const [tabs, setTabs] = useState(["Tab 1"]);
   const [hoveredTab, setHoveredTab] = useState<number | null>(null); //to control the styling of the tab we hover over
   const [activeTab, setActiveTab] = useState<number | null>(0);
+  console.log("tab no: ", activeTab);
 
   const addNewTab = () => {
-    setTabs([...tabs, `Tab ${tabs.length + 1}`]);
+    setTabs((tabs) => [...tabs, `Tab ${tabs.length + 1}`]);
     setActiveTab(tabs.length); // Set the newly added tab as active, remember tabs not updated yet, not until re render
   };
 
-  const closeTab = (index: number) => {
-    setTabs(tabs.filter((_, i) => i !== index));
-    if (index === activeTab) {
-      if (tabs.length === 1) {
-        addNewTab();
-      } else {
-        setActiveTab(tabs.length - 1); // If the tab to be closed is the active tab, set the previous tab as active
-      }
+  const closeTab = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    index: number
+  ) => {
+    event.stopPropagation(); // Stop the event from bubbling up to the parent div
+
+    setTabs((tabs) => tabs.filter((_, i) => i !== index));
+
+    if (tabs.length === 1) {
+      addNewTab();
+    } else if (index === activeTab) {
+      setActiveTab(activeTab - 1); // Set the last tab as active, remember tabs not updated yet,
+    } else {
+      setActiveTab(tabs.length - 2); // Keep the active tab as it is
     }
   };
 
@@ -34,15 +41,22 @@ const Tabs: React.FC = () => {
     <div className="flex-1 overflow-hidden">
       <div className="flex overflow-hidden items-center h-8">
         {tabs.map((tab, index) => (
-          //  outer div so that when you hover over tab, it does not colour all the way to tab border
+          // outer div so that when you hover over tab, it does not colour all the way to tab border
+          // active tab gets border on top that is rounded
+          // tabs other than one to left of active get a right border
           <div
             key={index}
-            className={`flex basis-40 justify-center items-center h-8 border-r px-1 border-gray-300 overflow-hidden ${
-              activeTab === index && "bg-white"
+            className={`flex basis-40 justify-center items-center h-8 px-1 border-gray-300 overflow-hidden ${
+              (activeTab === index &&
+                "bg-white border-t border-l border-r rounded-t-md") ||
+              (activeTab !== index + 1 && "border-r")
             } `}
           >
+            {/* if not active tab then become grey when we hover the tab */}
             <div
-              className="h-7 basis-36 flex items-center justify-between text-sm hover:bg-slate-300 rounded overflow-hidden"
+              className={`h-7 basis-36 flex items-center justify-between text-sm rounded overflow-hidden ${
+                activeTab !== index && "hover:bg-slate-300"
+              }`}
               onMouseEnter={() => setHoveredTab(index)}
               onMouseLeave={() => setHoveredTab(null)}
               onClick={() => setActiveTab(index)}
@@ -51,11 +65,11 @@ const Tabs: React.FC = () => {
               <span className="whitespace-nowrap overflow-hidden text-ellipsis">
                 {tab}
               </span>
+              {/* if this is the button we are hovering over, show close button */}
               {index === hoveredTab && (
                 <button
-                  // if this is the button we are hovering over, show close button
                   className="justify-center items-center flex-shrink-0"
-                  onClick={() => closeTab(index)}
+                  onClick={(e) => closeTab(e, index)}
                 >
                   <img
                     src={CloseIcon}
