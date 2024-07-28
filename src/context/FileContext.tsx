2319328e70cabfac173219e0a4c4de1ed72a70ce
Ruthvik Konduru <114494803+BRKon123@users.noreply.github.com>
2024-07-28 13:52:58 +0100

    update the context props interface name, this new version of file context manages meta information about all the files in the vault

--- src/context/FileContext.tsx
+++ src/context/FileContext.tsx
@@ -3,14 +3,14 @@ import React, { createContext, useState, ReactNode, useContext } from "react";
 import FileInfo from "../models/FileInfo";
 
 // Define the shape of the context state
-interface FileContextState {
+interface FileContextProps {
   files: FileInfo[];
   addFile: (file: FileInfo) => void;
   removeFile: (fileName: string) => void;
 }
 
 // Create the context with default values
-const FileContext = createContext<FileContextState | undefined>(undefined);
+const FileContext = createContext<FileContextProps | undefined>(undefined);
 
 // Create the provider component
 const FileProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
@@ -34,7 +34,7 @@ const FileProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 // Custom hook to use the FileContext
-const useFileContext = (): FileContextState => {
+const useFileContext = (): FileContextProps => {
   const context = useContext(FileContext);
   if (!context) {
     throw new Error("useFileContext must be used within a FileProvider");
